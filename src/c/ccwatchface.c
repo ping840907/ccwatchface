f9d308d83ba9315e09f4ee1ffd53a2c20f062e6f
ping840907 <tom840907@gmail.com>
2026-04-05 09:35:54 +0800

    更新 ccwatchface.c

--- src/c/ccwatchface.c
+++ src/c/ccwatchface.c
@@ -546,13 +546,8 @@ static void update_time_display(struct tm *tick_time) {
         if (hour == 0) hour = 12;
     }
 
-    uint32_t hour_tens = RESOURCE_ID_NONE;
-    uint32_t hour_ones = (hour == 0) ? RESOURCE_ID_IMG_U0 :
-                          TIME_UPPERCASE_ONES_RESOURCES[hour % 10];
-
-    if (hour > 10) {
-        hour_tens = TIME_UPPERCASE_TENS_RESOURCES[hour / 10];
-    }
+    uint32_t hour_tens = (hour == 10) ? RESOURCE_ID_NONE : TIME_UPPERCASE_TENS_RESOURCES[hour / 10];
+    uint32_t hour_ones = (hour == 0) ? RESOURCE_ID_IMG_U0 : TIME_UPPERCASE_ONES_RESOURCES[hour % 10];
 
     display_layer_update(&s_app.hour_layers[0], hour_tens);
     display_layer_update(&s_app.hour_layers[1], hour_ones);
